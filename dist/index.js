63f71e7f07f7e1f3aad9814d87286f0cd35b84c8
Atanas Atanasov <scriptex.bg@gmail.com>
2018-08-18 13:50:38 +0300

    Update build files

--- dist/index.js
+++ dist/index.js
@@ -1,154 +1,63 @@
-(function webpackUniversalModuleDefinition(root, factory) {
-	if(typeof exports === 'object' && typeof module === 'object')
-		module.exports = factory();
-	else if(typeof define === 'function' && define.amd)
-		define([], factory);
-	else if(typeof exports === 'object')
-		exports["passScore"] = factory();
-	else
-		root["passScore"] = factory();
-})(window, function() {
-return /******/ (function(modules) { // webpackBootstrap
-/******/ 	// The module cache
-/******/ 	var installedModules = {};
-/******/
-/******/ 	// The require function
-/******/ 	function __webpack_require__(moduleId) {
-/******/
-/******/ 		// Check if module is in cache
-/******/ 		if(installedModules[moduleId]) {
-/******/ 			return installedModules[moduleId].exports;
-/******/ 		}
-/******/ 		// Create a new module (and put it into the cache)
-/******/ 		var module = installedModules[moduleId] = {
-/******/ 			i: moduleId,
-/******/ 			l: false,
-/******/ 			exports: {}
-/******/ 		};
-/******/
-/******/ 		// Execute the module function
-/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
-/******/
-/******/ 		// Flag the module as loaded
-/******/ 		module.l = true;
-/******/
-/******/ 		// Return the exports of the module
-/******/ 		return module.exports;
-/******/ 	}
-/******/
-/******/
-/******/ 	// expose the modules object (__webpack_modules__)
-/******/ 	__webpack_require__.m = modules;
-/******/
-/******/ 	// expose the module cache
-/******/ 	__webpack_require__.c = installedModules;
-/******/
-/******/ 	// define getter function for harmony exports
-/******/ 	__webpack_require__.d = function(exports, name, getter) {
-/******/ 		if(!__webpack_require__.o(exports, name)) {
-/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
-/******/ 		}
-/******/ 	};
-/******/
-/******/ 	// define __esModule on exports
-/******/ 	__webpack_require__.r = function(exports) {
-/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
-/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
-/******/ 		}
-/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
-/******/ 	};
-/******/
-/******/ 	// create a fake namespace object
-/******/ 	// mode & 1: value is a module id, require it
-/******/ 	// mode & 2: merge all properties of value into the ns
-/******/ 	// mode & 4: return value when already ns object
-/******/ 	// mode & 8|1: behave like require
-/******/ 	__webpack_require__.t = function(value, mode) {
-/******/ 		if(mode & 1) value = __webpack_require__(value);
-/******/ 		if(mode & 8) return value;
-/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
-/******/ 		var ns = Object.create(null);
-/******/ 		__webpack_require__.r(ns);
-/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
-/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
-/******/ 		return ns;
-/******/ 	};
-/******/
-/******/ 	// getDefaultExport function for compatibility with non-harmony modules
-/******/ 	__webpack_require__.n = function(module) {
-/******/ 		var getter = module && module.__esModule ?
-/******/ 			function getDefault() { return module['default']; } :
-/******/ 			function getModuleExports() { return module; };
-/******/ 		__webpack_require__.d(getter, 'a', getter);
-/******/ 		return getter;
-/******/ 	};
-/******/
-/******/ 	// Object.prototype.hasOwnProperty.call
-/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
-/******/
-/******/ 	// __webpack_public_path__
-/******/ 	__webpack_require__.p = "";
-/******/
-/******/
-/******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = 0);
-/******/ })
-/************************************************************************/
-/******/ ([
-/* 0 */
-/***/ (function(module, exports, __webpack_require__) {
-
-"use strict";
-
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports.default = void 0;
-
-/**
- * Default array with regexp conditions
- * @type {Array}
- */
-var patterns = [/\d/, // Digits
-/[A-Z]/, // Uppercase letters
-/[\W_,!?@*\(\)]/, // Special characters
-/^.{8}/ // At least 8 characters
-];
-/**
- * Checks the score of a password input string
- * against an array of regexp conditions
- *
- * @param  {String} pass - Password as a string
- * @param  {Array}  conditions - Array of Regular Expressions
- *
- * @return {Number}
- */
-
-var passScore = function passScore(pass) {
-  var conditions = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : patterns;
-
-  // Convert undefined password to string
-  if (typeof pass === 'undefined') {
-    pass = '';
-  } // Convert everything to empty string
-
-
-  try {
-    pass = pass.toString();
-  } catch (e) {
-    // If null - convert to empty string
-    pass = '';
+(function (global, factory) {
+  if (typeof define === "function" && define.amd) {
+    define(["exports"], factory);
+  } else if (typeof exports !== "undefined") {
+    factory(exports);
+  } else {
+    var mod = {
+      exports: {}
+    };
+    factory(mod.exports);
+    global.index = mod.exports;
   }
+})(this, function (_exports) {
+  "use strict";
 
-  return conditions.reduce(function (accumulator, pattern) {
-    return accumulator + pattern.test(pass);
-  }, 0);
-};
+  Object.defineProperty(_exports, "__esModule", {
+    value: true
+  });
+  _exports.default = void 0;
 
-var _default = passScore;
-exports.default = _default;
+  /**
+   * Default array with regexp conditions
+   * @type {Array}
+   */
+  var patterns = [/\d/, // Digits
+  /[A-Z]/, // Uppercase letters
+  /[\W_,!?@*\(\)]/, // Special characters
+  /^.{8}/ // At least 8 characters
+  ];
+  /**
+   * Checks the score of a password input string
+   * against an array of regexp conditions
+   *
+   * @param  {String} pass - Password as a string
+   * @param  {Array}  conditions - Array of Regular Expressions
+   *
+   * @return {Number}
+   */
 
-/***/ })
-/******/ ])["default"];
+  var passScore = function passScore(pass) {
+    var conditions = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : patterns;
+
+    // Convert undefined password to string
+    if (typeof pass === 'undefined') {
+      pass = '';
+    } // Convert everything to empty string
+
+
+    try {
+      pass = pass.toString();
+    } catch (e) {
+      // If null - convert to empty string
+      pass = '';
+    }
+
+    return conditions.reduce(function (accumulator, pattern) {
+      return accumulator + pattern.test(pass);
+    }, 0);
+  };
+
+  var _default = passScore;
+  _exports.default = _default;
 });
